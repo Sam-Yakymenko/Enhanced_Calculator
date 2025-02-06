277e709ce553de2ab6dbc6084b17ff9828029073
Sam Yakymenko <sekazz24@gmail.com>
2025-02-06 16:01:17 +0200

    Easy one

---
+++ main.py
@@ -0,0 +1,13 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+oper = input("Enter the operator: ")
+
+if oper == '+':
+    print(a + b)
+elif oper == '-':
+    print(a - b)
+elif oper == '*':
+    print(a * b)
+elif oper == '/':
+    print(a / b)
+    
